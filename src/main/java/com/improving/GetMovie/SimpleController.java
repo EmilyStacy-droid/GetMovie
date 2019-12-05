97e1e7f128b41fe4234c50beb9225629d4f00af2
Emily Stacy <Emily.Stacy@improving.com>
2019-12-05 13:39:58 -0600

    simple controller not working

--- src/main/java/com/improving/GetMovie/SimpleController.java
+++ src/main/java/com/improving/GetMovie/SimpleController.java
@@ -1,4 +1,32 @@
 package com.improving.GetMovie;
 
+import com.improving.GetMovie.Intercepters.MovieRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+@Controller
 public class SimpleController {
+private MovieRepository movieRepository;
+
+    public SimpleController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+@RequestMapping(value="/movies",  method= RequestMethod.GET)
+    public String home(ModelMap model) {
+    model.put("movies", movieRepository.getMovies());
+    return "movies";
+}
+
+
+//private void setCommonModelAttribute(ModelMap model, Principal principal) {
+//    model.put("movies", movieRepository.getMovies());
+//}
+
+
 }
