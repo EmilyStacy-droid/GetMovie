cfb573675feace58212c6a13eb7fc3cce969ba38
Emily Stacy <Emily.Stacy@improving.com>
2019-12-05 08:19:24 -0600

    buildUpgradle

---
+++ src/main/java/com/improving/GetMovie/MVCConfig.java
@@ -0,0 +1,17 @@
+package com.improving.GetMovie;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Bean
+    public RestTemplate restTemplate (){
+        return new RestTemplateBuilder().build();
+    }
+
+}
