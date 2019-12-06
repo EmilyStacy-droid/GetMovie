b1f14cf99ffe98b94ebb40b8d655f9bea8f7e57f
Emily Stacy <Emily.Stacy@improving.com>
2019-12-06 08:51:50 -0600

    JPAWIP

--- src/main/java/com/improving/GetMovie/Intercepters/Movie.java
+++ src/main/java/com/improving/GetMovie/Intercepters/Movie.java
@@ -1,6 +1,16 @@
 package com.improving.GetMovie.Intercepters;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+@Id
+@GeneratedValue(strategy= GenerationType.AUTO)
+private Integer id;
+
 
     private final String movieTitle;
     private final int year;
